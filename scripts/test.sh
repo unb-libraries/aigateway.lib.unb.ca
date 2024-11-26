08802b0fcb91b5123d4e13bd0049b82e531f8746
Jacob Sanford <jsanford@unb.ca>
2024-11-25 21:38:43 -0400

    Update argon dependency

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 # Description: Test the API with a HTTP2 POST request
 
 curl \
-  --http2-prior-knowledge \
-  -H "x-pub-key: keyhere" \
-  -H "x-api-key: keyhere" \
+  --http1.1 \
+  -H "x-pub-key: 7d5acf3d-032d-4fe3-b24d-eccba2f53c8d" \
+  -H "x-api-key: 690c3205-54c3-4c09-8027-012708584db8" \
   -H "Content-Type: application/json" \
   --request POST \
   --data '{"query":"How big is the ocean?","context":""}' \
